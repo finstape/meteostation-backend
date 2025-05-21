b7aa1b3b8d24a17f913c9e848630452d216dcd93
finstape <finstape@mail.ru>
2025-05-21 20:18:05 +0300

    feat: add alembic support

--- app/db/alembic/env.py
+++ app/db/alembic/env.py
@@ -13,9 +13,11 @@ from app.db.models import weather
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-config.set_main_option("sqlalchemy.url", get_settings().database_uri)
+database_uri = get_settings().database_uri
 
-print(config.get_main_option("sqlalchemy.url"))
+if get_settings().USE_LOCAL_DB:
+    database_uri = database_uri.replace("@db", "@localhost")
+config.set_main_option("sqlalchemy.url", database_uri)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
