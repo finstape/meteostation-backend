6e8afeb9c7c578438cf98e0f5aeb8a3ff3ce309c
finstape <finstape@mail.ru>
2025-05-22 03:11:51 +0300

    feat: add table Settings

--- app/db/models/__init__.py
+++ app/db/models/__init__.py
@@ -1,6 +1,8 @@
+from app.db.models.setting import Setting
 from app.db.models.weather import Central, ExternalWeather, Outdoor
 
 __all__ = [
+    "Setting",
     "Central",
     "Outdoor",
     "ExternalWeather",
